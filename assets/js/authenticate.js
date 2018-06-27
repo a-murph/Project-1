78ba5920071933017925237898f83d037672db58
mpmcauliffe <mcauliffe99@gmail.com>
2018-06-26 20:02:31 -0400

    create auth code

---
+++ assets/js/authenticate.js
@@ -0,0 +1,14 @@
+/* ======================
+    AUTHENTICATE USER 
+=========================*/
+(function(){
+    const config = {
+        apiKey: "AIzaSyCEscL9E-9Hpe6QzKdzgQRYJXVaZia6_24",
+        authDomain: "hear-n-now.firebaseapp.com",
+        databaseURL: "https://hear-n-now.firebaseio.com",
+        projectId: "hear-n-now",
+        storageBucket: "hear-n-now.appspot.com",
+        messagingSenderId: "323599902659"
+    };
+    firebase.initializeApp(config);
+}());
